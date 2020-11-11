e5281400adde1eee359cd57d7341468da3d96f5b
md-ashraful-pramanik <1605001@ugrad.cse.buet.ac.bd>
2020-11-12 01:20:43 +0600

    Proj2 test case done

--- src/main/java/nachos/test/testcase.c
+++ src/main/java/nachos/test/testcase.c
@@ -1,9 +1,11 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include "syscall.h"
 
-int main(int argc, char** argv)
+
+int main()
 {
-    char[][] args;
+    char** args;
     printf("---------------INSIDE Testcase.c---------------\n");
     
     printf("---------------EXEC echo.coff---------------\n");
@@ -17,22 +19,34 @@ int main(int argc, char** argv)
     exec("halt.coff",0,args);
     printf("---------------MACHINE NOT HALTING---------------\n");
     
-    printf("---------------EXEC matmul.coff---------------\n");
-    int matmalPId = exec("matmul.coff",0,args);
-    
+    printf("---------------EXEC matmult.coff---------------\n");
+    int matmalPId = exec("matmult.coff",0,args);
+
     printf("---------------EXEC sort.coff---------------\n");
     int sortPId = exec("sort.coff",0,args);
-    
-    printf("---------------WAIT for matmul FINISH---------------\n");
+
+    printf("---------------WAIT for matmult FINISH---------------\n");
     int status = 0;
     status = join(matmalPId, &status);
-    printf("---------------JOINING from matmul.coff---------------\n");
-    printf("---------------MATMAL RETURN %d-----------------------\n", status);
-    
+    printf("---------------JOINING from matmult.coff---------------\n");
+
     printf("---------------WAIT for sort FINISH---------------\n");
     status = 0;
-    status = join(matmalPId, &status);
+    status = join(sortPId, &status);
     printf("---------------JOINING from sort.coff---------------\n");
+
+    printf("---------------WAIT for echo to FINISH---------------\n");
+    status = 0;
+    status = join(echoPId, &status);
+    printf("---------------JOINING from echo.coff---------------\n");
+
+    printf("---------------TRY TO EXEC matmult.coff Again---------------\n");
+    int matmalPId2 = exec("matmult.coff",0,args);
+
+    printf("---------------WAIT for matmult FINISH---------------\n");
+    int status2 = 0;
+    status2 = join(matmalPId2, &status2);
+    printf("---------------JOINING from matmult.coff---------------\n");
 
     printf("---------------Done Testcase.c---------------\n");
     return 0;
